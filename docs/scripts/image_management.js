cab711a47fe8a91b7f2244c42605fc0730f42f36
malteterboven <m-terboven@t-online.de>
2025-08-06 00:11:23 +0200

    Add calmont files

--- docs/scripts/image_management.js
+++ docs/scripts/image_management.js
@@ -4,6 +4,8 @@ const manageImageList = {
         "buildingBridges/1_visualisations/microhome-interior.jpg",
         "buildingBridges/1_visualisations/the-cabine.jpg",
         "calmont/1_visualisations/calmont-exterior.jpg",
+        "calmont/1_visualisations/Render-Foyer.jpg",
+        "calmont/1_visualisations/Render-Hof.jpg",
         "coralScraper/1_visualisations/coral-scraper.jpg",
         "school/1_visualisations/school-exterior.jpg"
     ],
@@ -11,9 +13,15 @@ const manageImageList = {
         "buildingBridges/plans/bridge-topography.jpg",
         "buildingBridges/plans/floor-plan.jpg",
         "buildingBridges/plans/modular-system.jpg",
+        "calmont/plans/Grundriss-EG.jpg",
+        "calmont/plans/Grundriss-OG.jpg",
+        "calmont/plans/Lageplan-500.jpg",
+        "calmont/plans/Schwarzplan-5000.jpg",
     ],
     sections: [
         "buildingBridges/sections/self-sufficient.jpg",
+        "calmont/sections/Schnitt-AA.jpg",
+        "calmont/sections/Schnitt-BB.jpg",
     ],
     pictograms: [
         "buildingBridges/pictograms/exploded-diagram.jpg",
@@ -21,10 +29,15 @@ const manageImageList = {
         "buildingBridges/pictograms/refugee-routes.jpg",
         "buildingBridges/pictograms/truckload.jpg",
     ],
-    evaluations: [
-
+    elevations: [
+        "calmont/elevations/Nordansicht.jpg",
+        "calmont/elevations/Ostansicht.jpg",
+        "calmont/elevations/Suedansicht.jpg",
+        "calmont/elevations/Westansicht.jpg",
     ],
     modelPhotos: [
+        "calmont/modelPhotos/Modell-200.jpg",
+        "calmont/modelPhotos/Modell-500.jpg",
 
     ],
 }
